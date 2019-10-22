8d0fbd17f2a7f45f57477d901d871716af6543f1
john94 <ethanok0802@gmail.com>
2019-10-22 09:05:19 -0700

    max/min drill twisted

--- drills.js
+++ drills.js
@@ -1,1 +1,30 @@
+'strict mode'
 
+function max(numbers) {
+    let maxNum = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        i++;
+        if (numbers[i] > maxNum) {
+            maxNum = numbers[i];
+        }
+    }
+    return maxNum;
+}
+
+function min(numbers) {
+    let maxNum = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        i++;
+        if (numbers[i] < maxNum) {
+            maxNum = numbers[i];
+        }
+    }
+    return maxNum;
+
+}
+
+console.log(max([-5, 28, 98, -20013, 0.7878, 22, 115]));
+
+console.log(min([-5, 28, 98, -20013, 0.7878, 22, 115]));
