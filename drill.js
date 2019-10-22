6ddb7a5aa4f150f51fb0e1cd20b49cfdc1d0e2ae
john94 <ethanok0802@gmail.com>
2019-10-22 12:35:12 -0700

    functions as return value

--- drill.js
+++ drill.js
@@ -43,3 +43,20 @@ const filteredNames = filter(myNames, function(name) {
 
 console.log(filteredNames) // => ['Rich', 'Ray']
     // <---- DO NOT EDIT BETWEEN THESE LINES
+
+function hazardWarningCreator(typeOfWarning) {
+    let warningCounter = 0;
+    return function(location) {
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`)
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+    };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
